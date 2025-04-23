d3a52e509ec6c0a001b652057b43b7ffe98d8a69
alenzo <alenzo.amico@gmail.com>
2025-04-23 11:00:09 +0200

    Update REDIRECT_URI to use the production URL for Spotify authentication

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 8888;
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-const REDIRECT_URI = "http://localhost:8888/callback";
+const REDIRECT_URI = "https://spotify-duel.onrender.com/callback";
 const SCOPES = [
   "user-library-read",
   "streaming",
